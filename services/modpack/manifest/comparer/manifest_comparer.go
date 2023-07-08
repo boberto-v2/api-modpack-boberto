41456bc7843813bec23bcb8ec3336f92c9eadc39
brutalzinn <robertocpaes@gmail.com>
2023-07-08 16:55:08 -0300

    add unit test to manifest comparer

--- services/modpack/manifest/comparer/manifest_comparer.go
+++ services/modpack/manifest/comparer/manifest_comparer.go
@@ -22,22 +22,54 @@ func New(old manifest_models.ManifestFiles, new manifest_models.ManifestFiles) M
 	return result
 }
 
-func (manifestComparer ManifestComparer) Compare() (result ManifestComparerResult) {
-	result.ToUpload = []manifest_models.ManifestFile{}
-	for _, oldFile := range manifestComparer.OldManifest.Files {
-		found := false
-		for _, newFile := range manifestComparer.NewManifest.Files {
-			if oldFile.Name != newFile.Name {
-				continue
-			}
-			if oldFile.Checksum != newFile.Checksum {
-				result.ToUpload = append(result.ToUpload, newFile)
-			}
-			found = true
-		}
-		if !found {
-			result.ToDelete = append(result.ToDelete, oldFile)
+// TODO: Show git blame to daniel to exlain why i refactor this.
+func (manifestComparer ManifestComparer) Compare() ManifestComparerResult {
+	result := ManifestComparerResult{
+		ToDelete: []manifest_models.ManifestFile{},
+		ToUpload: []manifest_models.ManifestFile{},
+	}
+	checksumsOld := make(map[uint32]bool)
+	for _, fileOld := range manifestComparer.OldManifest.Files {
+		checksumsOld[fileOld.Checksum] = true
+	}
+	for _, fileNew := range manifestComparer.NewManifest.Files {
+		if _, exists := checksumsOld[fileNew.Checksum]; !exists {
+			result.ToUpload = append(result.ToUpload, fileNew)
 		}
 	}
-	return
+	for _, fileOld := range manifestComparer.OldManifest.Files {
+		found := false
+		for _, fileNew := range manifestComparer.NewManifest.Files {
+			if fileNew.Checksum == fileOld.Checksum {
+				found = true
+				break
+			}
+		}
+		if !found {
+			result.ToDelete = append(result.ToDelete, fileOld)
+		}
+	}
+	return result
 }
+
+//TODO: Old comparer ( legacy and hard to read )
+
+// func (manifestComparer ManifestComparer) Compare() (result ManifestComparerResult) {
+// 	result.ToUpload = []manifest_models.ManifestFile{}
+// 	for _, oldFile := range manifestComparer.OldManifest.Files {
+// 		found := false
+// 		for _, newFile := range manifestComparer.NewManifest.Files {
+// 			if oldFile.Name != newFile.Name {
+// 				continue
+// 			}
+// 			if oldFile.Checksum != newFile.Checksum {
+// 				result.ToUpload = append(result.ToUpload, newFile)
+// 			}
+// 			found = true
+// 		}
+// 		if !found {
+// 			result.ToDelete = append(result.ToDelete, oldFile)
+// 		}
+// 	}
+// 	return
+// }
